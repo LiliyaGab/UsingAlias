ab899518c3807eb35d36388d657cfd3c1104771f
Liliya Gabitova <liliya.gabitova@jetbrains.com>
2023-09-15 15:59:09 +0200

    added project

---
+++ UsingAlias/UsingAlias/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UsingAlias
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

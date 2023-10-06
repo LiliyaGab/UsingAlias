c4a15371dd84da85e84c77a7691f05f1127c98bc
Liliya Gabitova <liliya.gabitova@jetbrains.com>
2023-10-06 12:17:13 +0200

    Added cases for QF "use type alias", "use namespace alias"

--- UsingAlias/ProjectReference/Class1.cs
+++ UsingAlias/ProjectReference/Class1.cs
@@ -1,7 +1,27 @@
 ﻿namespace ProjectReference
 {
-    public class Class1
+    public class Point
     {
+        private int x; // Do not rename (binary serialization)
+        private int y; // Do not rename (binary serialization)
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public int X
+        {
+            get => x;
+            set => x = value;
+        }
 
+        /// <summary>
+        /// Gets the y-coordinate of this <see cref='System.Drawing.Point'/>.
+        /// </summary>
+        public int Y
+        {
+            get => y;
+            set => y = value;
+        }
     }
 }
--- UsingAlias/UsingAlias_Core/Class.cs
+++ UsingAlias/UsingAlias_Core/Class.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using ForWpf = ProjectReference;
+using ForWinForms = System.Drawing;
+using NameValuePair = (string Name, object? Value);
 
 namespace UsingAlias_Core
 {
-    internal class Class
+    public class TreeChecker : ITreeProcessor<List<(string Name, object? Value)>> //use type alias
     {
+        public bool IsProcessFinished(List<(string Name, object? Value)> context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ProcessBeforeDescendants(INode element, List<(string Name, object? Value)> context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ProcessAfterDescendants(INode element, List<(string Name, object? Value)> context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface ITreeProcessor<T>
+    {
+        bool IsProcessFinished(List<(string Name, object? Value)> context);
+        void ProcessBeforeDescendants(INode element, List<(string Name, object? Value)> context);
+        void ProcessAfterDescendants(INode element, List<(string Name, object? Value)> context);
+    }
+
+    public interface INode
+    {
+    }
+
+    public class Converters
+    {
+        //use namespace alias
+        public static ProjectReference.Point Convert(System.Drawing.Point point) => new ProjectReference.Point(point.X, point.Y);
     }
 }

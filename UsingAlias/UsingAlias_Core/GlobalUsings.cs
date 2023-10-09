355ca01ddbd98abf0f9f15c49deddf8e6d789ec8
Liliya Gabitova <liliya.gabitova@jetbrains.com>
2023-10-09 13:14:34 +0200

    Added cases

---
+++ UsingAlias/UsingAlias_Core/GlobalUsings.cs
@@ -0,0 +1,4 @@
+// Global using directives
+
+global using System;
+global using ArrInt = int[]; //inline global using
--- UsingAlias/UsingAlias_Core/Program.cs
+++ UsingAlias/UsingAlias_Core/Program.cs
@@ -1,18 +1,19 @@
-﻿using unsafe VoidTest = void*;
+﻿using System.Collections;
+using System.Numerics;
+using unsafe VoidTest = void*;
 using unsafe UnsafeInt = int*;
 using unsafe PPint = int**;
-using unsafe ArrOfPointers = int*[];
+using unsafe Arr = int*[];
 using TupleNamed = (bool flag, double num, string str);
-using TupleIntString = (int, string);
+//using TupleIntString = (int, string);
 using unsafe sp = System.Span<int>*;
 using D = dynamic[];
-
+using unsafe UnsafeT = System.Numerics.BigInteger; //remove unsafe modifier
 
 namespace UsingAlias
 {
     using TupleIntString = (int, string); //use this type alias where possible
     using TupleIS = (int, string);
-    using ArrInt = int[];
     using TupleRef = Tuple<int, int>;
     using ArrMulti = double[,,];
     using ArrJagged = char[][];
@@ -21,6 +22,9 @@ namespace UsingAlias
     using NRTInside = List<string?>;
     using Dyn = dynamic;
     using unsafe sp = Span<int>*;
+    using static UnsafeT;
+    //using UnsafeInt = int*; //Make using directive unsafe
+    //unsafe using Test = string;
     using UserType = TypeWithLongNamewefsfsdvfsdvgsdgadsfwarweqfdshvbjhdbvsbdjfsdkhfkahsdjkcnjnxjnckjsdhfw;
     using unsafe TypeUnsafe = TypeWithLongNamewefsfsdvfsdvgsdgadsfwarweqfdshvbjhdbvsbdjfsdkhfkahsdjkcnjnxjnckjsdhfw*;
     using unsafe D = delegate* unmanaged[Stdcall, SuppressGCTransition]<int, int>;
@@ -28,7 +32,7 @@ namespace UsingAlias
     using unsafe Deleg = delegate*<delegate* managed<string, int>, delegate*<string, int>>;
     using unsafe EnumT = MyEnum;
     using static unsafe TypeWithLongNamewefsfsdvfsdvgsdgadsfwarweqfdshvbjhdbvsbdjfsdkhfkahsdjkcnjnxjnckjsdhfw;
-    using static Instance;
+    //using unsafe static Instance; //Move 'unsafe' modifier after 'static'
 
     internal class Program
     {
@@ -38,8 +42,34 @@ namespace UsingAlias
             //InstanceMethod();
             TypeWithLongNamewefsfsdvfsdvgsdgadsfwarweqfdshvbjhdbvsbdjfsdkhfkahsdjkcnjnxjnckjsdhfw t =
                 new TypeWithLongNamewefsfsdvfsdvgsdgadsfwarweqfdshvbjhdbvsbdjfsdkhfkahsdjkcnjnxjnckjsdhfw();
+            ArrInt array = new int[] { };
+            UnsafeT variable = Zero;
+        }
+
+
+        public void tt()
+        {
+            unsafe
+            {
+                static void Foo(List<Arr> arg)
+                {
+                        Bar(arg); //create from usage
+                }
+            }
+        }
+
+        private static unsafe void Bar(List<Arr> intsList)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        static void DoSmth(List<int> list, UserType userType) //change signature
+        {
 
         }
+
+
     }
     public delegate void FooBarDelegate(String x);
 
@@ -50,17 +80,70 @@ namespace UsingAlias
         public static void Method() {}
     }
 
-    public class CheckUseThisTypeAlias
+    public class CheckUseThisTypeAlias //generate code
     {
         private (int, string) tuple;
+
+        public CheckUseThisTypeAlias(Util u)
+        {
+            this.u = u;
+        }
+
+        public int CompareTo(object? other, IComparer comparer)
+        {
+            return ((IStructuralComparable)tuple).CompareTo(other, comparer);
+        }
+
+        public int Length => ((System.Runtime.CompilerServices.ITuple)tuple).Length;
+
+        private Util u;
+
+        protected bool Equals(CheckUseThisTypeAlias other)
+        {
+            return tuple.Equals(other.tuple) && u.Equals(other.u);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((CheckUseThisTypeAlias)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(tuple, u);
+        }
+
+        public Util U1
+        {
+            get => u;
+            set => u = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public (int, string) Tuple => tuple;
+
+        public Util U => u;
+
+        public unsafe void Deconstruct(out D del)
+        {
+            u.Deconstruct(out del);
+        }
 
         public void Method()
         {
             tuple = (3, "6");
         }
     }
-    unsafe class Util
+    public unsafe class Util
     {
+        public void Deconstruct(out D del)
+        {
+            del = Del;
+        }
+
+        private D Del;
         public static void Log() { }
 
         void Use()

9973b0f3bacb07f9a56714cedd607c05911749fc
Liliya Gabitova <liliya.gabitova@jetbrains.com>
2023-11-08 12:49:25 +0100

    Added package project and references

---
+++ UsingAlias/PackageProject/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace PackageProject
+{
+    public class PackageClass
+    {
+        public string Field { get; }
+        public int Prop { get; }
+
+        public PackageClass(string field, int prop)
+        {
+            this.Field = field;
+            this.Prop = prop;
+        }
+    }
+}
---
+++ UsingAlias/UsingAlias_Core/PackageUsage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PackageProject;
+using Pack = PackageProject.PackageClass;
+using T = (PackageProject.PackageClass, SharedProject.Class1);
+
+namespace UsingAlias_Core
+{
+    using Tt = (Pack, PackageClass);
+    internal class PackageUsage
+    {
+        private T tesTuple;
+        private Tt tTuple;
+
+        public void Method()
+        {
+            var item1 = tesTuple.Item1.Field + tesTuple.Item2.Tm.Item1;
+            var item2 = tTuple.Item1.Prop + tTuple.Item2.Prop;
+        }
+    }
+}

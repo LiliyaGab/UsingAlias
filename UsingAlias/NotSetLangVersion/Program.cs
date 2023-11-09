e46ce9b94fcdb406bdee6ad34a8c25320b5ccdc6
Liliya Gabitova <liliya.gabitova@jetbrains.com>
2023-11-09 10:33:43 +0100

    added more constructions to project without language version

--- UsingAlias/NotSetLangVersion/Program.cs
+++ UsingAlias/NotSetLangVersion/Program.cs
@@ -1,6 +1,18 @@
 ﻿using TupleTest = (bool, char); // Quick fixes - set language version
 using Tuple2 = int[];
 using unsafe Arr = int*[]; // TODO: add possible constructions
+using unsafe UnsafeInt = int*;
+using ArrMulti = double[,,];
+using ArrJagged = char[][]; 
+using NVT = int?;
+using NRTInside = System.Collections.Generic.List<string?>;
+using Dyn = dynamic;
+using unsafe Deleg = delegate*<delegate* managed<string, int>, delegate*<string, int>>;
+using unsafe VoidTest = void*; 
+using unsafe UnsafeInt = int*; 
+using unsafe PPint = int**;
+using TupleNamed = (bool flag, double num, string str);
+using D = dynamic[];
 
 namespace NotSetLangVersion
 {

88b0b37b97d362102d8944fb734d8a40a0b0e7e8
Liliya Gabitova <liliya.gabitova@jetbrains.com>
2023-11-13 15:44:34 +0100

    added types usages

--- UsingAlias/UsingAlias_Core/Completion.cs
+++ UsingAlias/UsingAlias_Core/Completion.cs
@@ -1,4 +1,13 @@
 ﻿//using Text // TODO: completion of using alias
+//using Text =  //types
+//using static unsafe 
+//using unsafe 
+//using unsafe D = 
+using unsafe PPint = int**;
+using ArrMulti = double[,,];
+using ArrJagged = char[][];
+using NVT = int?;
+using unsafe Deleg = delegate*<void> ;
 
 using TParam = (int, string);
 namespace UsingAlias_Core;
@@ -12,23 +21,40 @@ public class Generic
 
 	public void Test2()
 	{
-		TParam t = GetTParamTuple();
+        unsafe
+        {
+            TParam t = GetTParamTuple();
 		
 
-		TParam GetTParamTuple()
-		{
-			return new(1, "Jane Doe");
-		}
+            TParam GetTParamTuple()
+            {
+                return new(1, "Jane Doe");
+            }
 
 
-		int t2 = 1;
+            int t2 = 1;
 
-		//(int, string) t3 = ; // TODO: smart completion
+            //(int, string) t3 = ; // TODO: smart completion
 
 
-		int GetInt()
-		{
-			return 1;
-		}
-	}
+            int GetInt()
+            {
+                return 1;
+            }
+
+            //ArrInt arrInts = 
+            //TParam testValueTuple = 
+            //PPint
+            NVT i = t2;
+            //ArrJagged chars = new ArrJagged[][];
+            Deleg p = (Deleg)(&i);
+        }
+    }
+
+    public TParam GetTParamTuple2()
+    {
+        (int, string) t = (5, "tsrt");
+        TParam teTuple = (234324, "string");
+		//return 
+    }
 }
--- UsingAlias/UsingAlias_Core/Program.cs
+++ UsingAlias/UsingAlias_Core/Program.cs
@@ -21,7 +21,7 @@ namespace UsingAlias
     using TupleIntString = (int, string); //Context actions - inline type alias
     using TupleIS = (int, string); //Types - tuple unnamed // TODO: add usages
     using TupleRef = Tuple<int, int>;
-    using ArrMulti = double[,,]; //Types - array - multidimensional
+    using ArrMulti = double[,]; //Types - array - multidimensional
     using ArrJagged = char[][]; //Types - array - jagged
     using NVT = int?; //Types - nullable value type
     //using NRT = string?;  //Types - nullable reference type - top-level (not supported)
@@ -31,44 +31,56 @@ namespace UsingAlias
     using static UnsafeT; //Support of types containing pointers - using static
     //using UnsafeInt = int*; //Quick-fixes for unsafe - Make using directive unsafe
     //unsafe using Test = string;
-    using UserType = TypeWithLongNamewefsfsdvfsdvgsdgadsfwarweqfdshvbjhdbvsbdjfsdkhfkahsdjkcnjnxjnckjsdhfw; //Types - user defined type // TODO: in other file, namespace, project
-    using unsafe TypeUnsafe = TypeWithLongNamewefsfsdvfsdvgsdgadsfwarweqfdshvbjhdbvsbdjfsdkhfkahsdjkcnjnxjnckjsdhfw*; //Support of types containing pointers - custom type
-    using unsafe D = delegate* unmanaged[Stdcall, SuppressGCTransition]<int, int>; //Support of types containing pointers - delegate
+    using UserType =
+        TypeWithLongNamewefsfsdvfsdvgsdgadsfwarweqfdshvbjhdbvsbdjfsdkhfkahsdjkcnjnxjnckjsdhfw; //Types - user defined type // TODO: in other file, namespace, project
+    using unsafe TypeUnsafe =
+        TypeWithLongNamewefsfsdvfsdvgsdgadsfwarweqfdshvbjhdbvsbdjfsdkhfkahsdjkcnjnxjnckjsdhfw
+        *; //Support of types containing pointers - custom type
+    using unsafe D =
+        delegate* unmanaged[Stdcall, SuppressGCTransition]<int, int>; //Support of types containing pointers - delegate
     using unsafe Del = delegate* unmanaged<string, delegate*<int>>; //Support of types containing pointers - delegate
-    using unsafe Deleg = delegate*<delegate* managed<string, int>, delegate*<string, int>>; //Support of types containing pointers - delegate
+    using unsafe Deleg =
+        delegate*<delegate* managed<string, int>, delegate*<string, int>
+        >; //Support of types containing pointers - delegate
     using unsafe EnumT = MyEnum*; //Support of types containing pointers - enum
-    using static unsafe TypeWithLongNamewefsfsdvfsdvgsdgadsfwarweqfdshvbjhdbvsbdjfsdkhfkahsdjkcnjnxjnckjsdhfw; //Support of types containing pointers - using static
+    using static unsafe
+        TypeWithLongNamewefsfsdvfsdvgsdgadsfwarweqfdshvbjhdbvsbdjfsdkhfkahsdjkcnjnxjnckjsdhfw; //Support of types containing pointers - using static
     //using unsafe static Instance; //Quick-fixes for unsafe - Move 'unsafe' modifier after 'static'
 
 
     class Usages
     {
-	    // TODO: usages of named tuple alias
-	    public void
-		    UseNamedTuple(TupleNamed namedTuple,
-			    (bool flag, double num, string str) tuple2) // TODO: Inline alias CA: why not available on usage?
-	    {
-		    namedTuple = (false, 1, "Jane Doe"); // TODO: inlay hints of tuple's component names.
-		    namedTuple = (flag: false, num: 1, str: "Jane Doe"); // TODO: Rename on component name.
-		    namedTuple = (flag2: false, num: 1, str: "Jane Doe"); // TODO: not matching name inspection
-		    tuple2 = (flag: false, num: 1, str: "Jane Doe");
+        // TODO: usages of named tuple alias
+        public void
+            UseNamedTuple(TupleNamed namedTuple,
+                (bool flag, double num, string str) tuple2) // TODO: Inline alias CA: why not available on usage?
+        {
+            namedTuple = (false, 1, "Jane Doe"); // TODO: inlay hints of tuple's component names.
+            namedTuple = (flag: false, num: 1, str: "Jane Doe"); // TODO: Rename on component name.
+            namedTuple = (flag2: false, num: 1, str: "Jane Doe"); // TODO: not matching name inspection
+            tuple2 = (flag: false, num: 1, str: "Jane Doe");
 
-	    }
+        }
 
-	    // TODO: usages of unnamed tuple alias
-	    public TupleIS UseUnnamedTuple(TupleIS tuple)
-	    {
-		    tuple.Item1 = 1;
-		    tuple.Item2 = "Jane Doe";
-		    //tuple = (1, "Jane Doe", 2); // TODO: unmatched types
-		    //tuple = ("Jane Doe", 1);
-		    return tuple;
-	    }
+        // TODO: usages of unnamed tuple alias
+        public TupleIS UseUnnamedTuple(TupleIS tuple)
+        {
+            tuple.Item1 = 1;
+            tuple.Item2 = "Jane Doe";
+            //tuple = (1, "Jane Doe", 2); // TODO: unmatched types
+            //tuple = ("Jane Doe", 1);
+            return tuple;
+        }
 
-	    public ArrInt UseSingleArray(ArrInt? arrInt, ArrInt normalArray) // TODO: fix nullability mismatch
-	    {
+        public ArrInt UseSingleArray(ArrInt? arrInt, ArrInt normalArray) // TODO: fix nullability mismatch
+        {
             // TODO: check postfix templates are available and correct
             //arrInt.for
+            //arrInt.forr
+            //arrInt.foreach
+            //arrInt.notnull
+            //arrInt.null
+            //arrInt.switch
 
             arrInt = new[] { 1, 2, 3, 4 };
             normalArray = new[] { 1, 2, 3 };
@@ -79,8 +91,41 @@ namespace UsingAlias
 
 
             return arrInt;
-	    }
-}
+        }
+
+        public void UseUnsafe(int i)
+        {
+            UserType exp = new UserType();
+            unsafe
+            {
+                VoidTest tVoid = &i;
+                UnsafeInt iUnsafe = (UnsafeInt)tVoid;
+                PPint ppint = (PPint)iUnsafe;
+                TypeUnsafe typeWith = &exp;
+                Deleg deleg;
+            }
+        }
+
+        public void UseMultiArray()
+        {
+            ArrMulti arDoubles = { { 1.2, 2.3, 3.3 }, { 3.2, 3.2, 3.2 } };
+            ArrJagged arChars = new char[4][];
+            arChars[0] = new char[4];
+        }
+
+        public void UseDynamic()
+        {
+            Dyn dyn_ex = new Class1(("", false));
+            D dynex = dyn_ex;
+        }
+
+        public void UseOther()
+        {
+            NVT v = new NVT();
+            NRTInside list = new NRTInside();
+            list.Add(null);
+        }
+    }
 
     internal class Program
     {
@@ -90,9 +135,10 @@ namespace UsingAlias
             //InstanceMethod();
             TypeWithLongNamewefsfsdvfsdvgsdgadsfwarweqfdshvbjhdbvsbdjfsdkhfkahsdjkcnjnxjnckjsdhfw t =
                 new TypeWithLongNamewefsfsdvfsdvgsdgadsfwarweqfdshvbjhdbvsbdjfsdkhfkahsdjkcnjnxjnckjsdhfw();
-			ArrInt array = new int[] { };
-			UnsafeT variable = Zero;
-            System.Security.Cryptography.Aes tAes = Aes.Create(); //Namespace imports page - no suggestion to remove namespace
+            ArrInt array = new int[] { };
+            UnsafeT variable = Zero;
+            System.Security.Cryptography.Aes
+                tAes = Aes.Create(); //Namespace imports page - no suggestion to remove namespace
             System.Security.SecureString secureString = new System.Security.SecureString(); //remove redundant
             int tempCompare = string.Compare("a", 4, "b", 5, 7, CultureInfo.CurrentCulture, CompareOptions.IgnoreCase);
             Class1 tClass1 = new Class1(("", true))
@@ -104,18 +150,18 @@ namespace UsingAlias
 
         public void tt()
         {
-	        unsafe
-	        {
-		        static void Foo(List<Arr> arg)
-		        {
-			        Bar(arg); //R# features - create from usage unsafe
-		        }
-	        }
+            unsafe
+            {
+                static void Foo(List<Arr> arg)
+                {
+                    Bar(arg); //R# features - create from usage unsafe
+                }
+            }
         }
 
         private static unsafe void Bar(List<Arr> intsList)
         {
-	        throw new NotImplementedException();
+            throw new NotImplementedException();
         }
 
 
@@ -131,13 +177,16 @@ namespace UsingAlias
 
 
     }
+
     public delegate void FooBarDelegate(String x);
 
     public class TypeWithLongNamewefsfsdvfsdvgsdgadsfwarweqfdshvbjhdbvsbdjfsdkhfkahsdjkcnjnxjnckjsdhfw
     {
         private TupleIntString t = (10, "sfsff");
 
-        public static void Method() {}
+        public static void Method()
+        {
+        }
     }
 
     public class CheckUseThisTypeAlias //R features - generate code
@@ -196,6 +245,7 @@ namespace UsingAlias
             tuple = (3, "6");
         }
     }
+
     public unsafe class Util
     {
         public void Deconstruct(out D del)
@@ -204,13 +254,17 @@ namespace UsingAlias
         }
 
         private D Del;
-        public static void Log() { }
+
+        public static void Log()
+        {
+        }
 
         void Use()
         {
             delegate*<void> ptr1 = &Util.Log; //Context actions - introduce type alias on 'delegate*<void>'
         }
     }
+
     unsafe class Instance
     {
         void Use()
@@ -220,11 +274,19 @@ namespace UsingAlias
             f(this);
         }
 
-        public static void InstanceMethod() { }
+        public static void InstanceMethod()
+        {
+        }
     }
 
     enum MyEnum
     {
-        value1, value2, value3, value4, value5, value6
+        value1,
+        value2,
+        value3,
+        value4,
+        value5,
+        value6
     }
 }
+

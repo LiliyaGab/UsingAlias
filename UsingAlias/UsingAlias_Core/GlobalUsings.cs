7ba304000781bee2c7bd8f7cbfef48f33fc4fdec
Liliya Gabitova <liliya.gabitova@jetbrains.com>
2023-11-16 13:39:51 +0100

    added case to check settings

--- UsingAlias/UsingAlias_Core/GlobalUsings.cs
+++ UsingAlias/UsingAlias_Core/GlobalUsings.cs
@@ -1,4 +1,4 @@
 // Global using directives
 
-global using System;
+//global using System;
 global using ArrInt = int[]; //Types - array - single dimensional //R# features - Refactorings - Convert to non-global using //TODO: Rename to Arr (already existing), to not existing name.
--- UsingAlias/UsingAlias_Core/SomeClass.cs
+++ UsingAlias/UsingAlias_Core/SomeClass.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using UnityEngine;
+//using Random = System.Random;
 
 namespace UsingAlias
 {
@@ -13,6 +10,8 @@ namespace UsingAlias
 
         public void Method()
         {
+            var rnd = UnityEngine.Random.Range(0,100); 
+            Random rand = new Random(); //use class qf with enabled 'use using alias directive to resolve conflicts'
             Console.WriteLine(T);
         }
     }
